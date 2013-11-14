8f3f82abee98d040a5230bf8d32825614e221571
Kjetil Midtlie <kdm@docstream.no>
2013-11-14 10:49:08 +0100

    klar for test

--- upload2maven_repo.sh
+++ upload2maven_repo.sh
@@ -1,3 +1,22 @@
-#!/bin/make -f
+#!/bin/bash
 
-scp 
+DEFAULT_SRV=packages_prod
+DEFAULT_DIRDEST=.
+SRC=work/maven_vendor_all.tar.gz
+
+if [ ! -f $SRC ]
+then
+  echo finner ikke $SRC ! .. bygg den først
+fi
+
+echo
+echo Klar for upload av $SRC
+echo
+
+read -p "Hva er server navn (packages_prod) :" SRV_INP
+read -p "Hva mappe destinasjon (.) :" DIRDEST_INP
+
+SRV=${SRV_INP:-${DEFAULT_SRV}}
+DIRDEST=${DIRDEST_INP:-${DEFAULT_DIRDEST}}
+
+scp $SRC $SRV:$DIRDEST 
